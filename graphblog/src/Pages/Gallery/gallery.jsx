b56b94deedbb0c7570ea9930e2b38bde9909a41c
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-04 14:23:46 +0200

    update

--- graphblog/src/Pages/Gallery/gallery.jsx
+++ graphblog/src/Pages/Gallery/gallery.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import { GraphQLClient } from 'graphql-request';
+import request from "graphql-request";
+import { Link, useParams } from "react-router-dom";
+// import { getGallerys } from "../Query/getGallery";
 
-const Gallery = () => {
+export const Gallery = () => {
+
+  const { id } = useParams();
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["gallery"],
+    queryFn: async () =>
+      request(
+        "",
+        getGallery,
+        { personId: id }
+      ),
+  });
 
   return (
     
