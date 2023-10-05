f1b556e4a9a8f889d19d5166dafb3a4f2b82c5a6
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-05 15:25:26 +0200

    more styling

--- graphblog/src/Pages/HomePage/homePage.jsx
+++ graphblog/src/Pages/HomePage/homePage.jsx
@@ -4,10 +4,16 @@ import React from 'react';
 const Homepage = () => {
   return (
     <div>
-      <h2>Welcome to My Blog</h2>
+      <h2>Welcome to Our Dog Blog
+</h2>
       <p>
-        This is the homepage of my blog. You can start exploring the content by
-        navigating through the menu or reading the latest articles below.
+
+Explore the fascinating world of our four-legged companions through 
+heartwarming stories, helpful tips, and captivating adventures. 
+Our dog blog is dedicated to celebrating the bond between humans and their furry friends.
+ Join us on a journey filled with tail-wagging tales, expert insights, 
+ and everything you need to know about being the best pet parent. Whether you're a 
+ seasoned dog lover or a new pup enthusiast, our blog has something for everyone. Get ready to embark on an adventure of love, laughter, and endless pawprints.
       </p>
 
       <h3>Latest Articles</h3>
