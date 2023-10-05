fd65b4bf85796653aa174ab46f9c2de3a765513e
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-05 14:41:34 +0200

    griddy gallery

--- graphblog/src/Pages/Gallery/gallery.jsx
+++ graphblog/src/Pages/Gallery/gallery.jsx
@@ -1,34 +1,33 @@
-import {request}  from 'graphql-request'
-import { useQuery } from "@tanstack/react-query";
-import { Link, useParams } from "react-router-dom";
-import { getGallery } from "../../Queries/getGallery";
+import { request } from 'graphql-request';
+import { useQuery } from '@tanstack/react-query';
+import { Link, useParams } from 'react-router-dom';
+import { getGallery } from '../../Queries/getGallery';
+import style from './gallery.module.scss';
 
 export const Gallery = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['giveMeTheGallery'],
+    queryFn: async () => request(import.meta.env.VITE_PUBLIC_URL_ID, getGallery),
+  });
 
-    const { data, isLoading, error } = useQuery({
-      queryKey: ["giveMeTheGallery"],
-      queryFn: async () => request(import.meta.env.VITE_PUBLIC_URL_ID, getGallery),
-    });
-    console.log(data)
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
 
-    if (isLoading) {
-      return <p>Loading... </p>;
-    }
-  
-    if (error) {
-      return <p>Error: {error.message}</p>;
-    }
-  
-    return (
-      <div>
+  return (
+    <div>
+      <h2 className={style.doggyTitle}>Doggy Gallery</h2>
+      <div className={style.doggyGrid}>
         {data.galleries.map((gallery, index) => (
-          <div key={index}>
-            <img src={gallery.dog.url} alt={`Dog ${index + 1}`} />
+          <div key={index} className={style.doggyImgContainer}>
+            <img src={gallery.dog.url} alt={`Dog ${index + 1}`} className={style.doggyImg} />
           </div>
         ))}
       </div>
-    );
-  };
-
-export default Gallery;
+    </div>
+  );
+};
