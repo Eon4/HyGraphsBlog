e637c552efa2ba05e15745aaad7f7f43fbdea8a3
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-10-06 13:35:31 +0200

    Update sorting

--- graphblog/src/Pages/BlogPage/blogPage.jsx
+++ graphblog/src/Pages/BlogPage/blogPage.jsx
@@ -1,7 +1,7 @@
 import { request } from 'graphql-request';
 import { useQuery } from '@tanstack/react-query';
 import { getBlockPosts } from '../../Queries/getBlockPosts';
-import React from 'react';
+import React, { useState } from 'react';
 import style from '../../Pages/BlogPage/blockPage.module.scss';
 
 export const Blog = () => {
@@ -11,7 +11,8 @@ export const Blog = () => {
       request(import.meta.env.VITE_PUBLIC_URL_ID, getBlockPosts),
   });
 
-  console.log(data);
+  const [sortingOption, setSortingOption] = useState('date'); // Default sorting by date
+  const [displayLatestPosts, setDisplayLatestPosts] = useState(false);
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -23,17 +24,75 @@ export const Blog = () => {
 
   const posts = data?.posts || [];
 
+  // Function to handle sorting
+  const handleSorting = (option) => {
+    setSortingOption(option);
+
+    // Sort the posts based on the selected option
+    if (option === 'date') {
+      posts.sort((a, b) => new Date(b.dateAndTime) - new Date(a.dateAndTime));
+    } else if (option === 'name') {
+      posts.sort((a, b) => a.title.localeCompare(b.title));
+    }
+  };
+
+  // Function to filter posts by the current date
+  const filterLatestPosts = () => {
+    const currentDate = new Date().toLocaleDateString();
+    const latestPosts = posts.filter(
+      (post) => new Date(post.dateAndTime).toLocaleDateString() === currentDate
+    );
+    return latestPosts;
+  };
+
+  const postsToDisplay = displayLatestPosts ? filterLatestPosts() : posts;
+  const getLatestPostSnippet = () => {
+    if (posts.length > 0) {
+      const latestPost = posts[0]; // Get the latest post
+      return (
+        <div className={style.LatestPostSnippet}>
+          <h3>Latest Article:</h3>
+          <Link to={`/blogPage/${latestPost.id}`} className={style.LatestPostLink}>
+            {latestPost.title}
+          </Link>
+          <p className={style.LatestPostDescription}>{latestPost.description}</p>
+        </div>
+      );
+    }
+    return null; // Return null if no posts
+  };
+
   return (
     <div className={style.BlogContainer}>
       <h2 className={style.BlogTitle}>Blog Posts</h2>
-      {posts.length === 0 ? (
+      
+      <div className={style.FiltersAndButton}>
+        <div className={style.Filters}>
+          {/* Dropdown menu for sorting */}
+          <select onChange={(e) => handleSorting(e.target.value)} className={style.SortingDropdown}>
+            <option value="date">Sort by Date</option>
+            <option value="name">Sort by Name</option>
+          </select>
+        </div>
+
+        <button
+          onClick={() => setDisplayLatestPosts(!displayLatestPosts)}
+          className={style.LatestPostsButton}
+        >
+          {displayLatestPosts
+            ? 'Show All Posts'
+            : 'Show Latest Posts from Today'}
+        </button>
+      </div>
+
+      {postsToDisplay.length === 0 ? (
         <p className={style.NoPosts}>No blog posts available.</p>
       ) : (
         <ul className={style.PostList}>
-          {posts.map((post, index) => (
+          {postsToDisplay.map((post, index) => (
             <li key={index} className={style.PostItem}>
-       <h1 className={style.PostTitle}>{post.title}</h1>
-       <p className={style.Date}> Posted: {post.dateAndTime}</p>
+              <h1 className={style.PostTitle}>{post.title}</h1>
+              <p className={style.Date}>Posted: {post.dateAndTime}</p>
 
               <img
                 src={post.coverPhoto.url}
@@ -48,4 +107,3 @@ export const Blog = () => {
     </div>
   );
 };
-
--- graphblog/src/Pages/HomePage/homePage.jsx
+++ graphblog/src/Pages/HomePage/homePage.jsx
@@ -1,25 +1,30 @@
-
 import React from 'react';
+import { Link } from 'react-router-dom';
+import style from '../../Pages/HomePage/homePage.module.scss'; // Make sure to import the correct SCSS module
 
 const Homepage = () => {
   return (
-    <div>
-      <h2>Welcome to Our Dog Blog
-</h2>
+    <div className={style.HomepageContainer}>
+      <h2>Welcome to Our Dog Blog</h2>
       <p>
-
-Explore the fascinating world of our four-legged companions through 
-heartwarming stories, helpful tips, and captivating adventures. 
-Our dog blog is dedicated to celebrating the bond between humans and their furry friends.
- Join us on a journey filled with tail-wagging tales, expert insights, 
- and everything you need to know about being the best pet parent. Whether you're a 
- seasoned dog lover or a new pup enthusiast, our blog has something for everyone. Get ready to embark on an adventure of love, laughter, and endless pawprints.
+        Explore the fascinating world of our four-legged companions through
+        heartwarming stories, helpful tips, and captivating adventures. Our dog
+        blog is dedicated to celebrating the bond between humans and their furry
+        friends. Join us on a journey filled with tail-wagging tales, expert
+        insights, and everything you need to know about being the best pet
+        parent. Whether you're a seasoned dog lover or a new pup enthusiast,
+        our blog has something for everyone. Get ready to embark on an adventure
+        of love, laughter, and endless pawprints.
       </p>
 
-      <h3>Latest Articles</h3>
- 
+      <h3>
+        <Link to="/blogPage" className={style.LatestArticlesLink}>
+          Latest Blogs
+        </Link>
+      </h3>
     </div>
   );
 };
 
 export default Homepage;
+
